86da39b932f2a479a6894f61567371790588e4f2
АртемPC <Sjmavrik@gmail.com>
2023-12-10 21:27:40 +0300

    Добавление Шаблона HTML

--- website/main/views.py
+++ website/main/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h4> Проверка работы </h4>')
+    return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse('<h4> Страница про нас </h4>')
+    return render(request, 'main/about.html')
